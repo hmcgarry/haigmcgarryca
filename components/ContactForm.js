225da8e68654172b0b953a0f32220ecce364a992
Haig McGarry <mcgarry.haig@gmail.com>
2026-01-09 14:58:11 -0500

    replaced email with formsubmit string

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,6 +1,6 @@
 export default function ContactForm(){
     return(
-    <form action="https://formsubmit.co/mcgarry.haig@gmail.com" method='POST'>
+    <form action="https://formsubmit.co/e1ce23fb350fd46fa60e91af659afd3b" method='POST'>
         <input type="text" name="firstname" required/>
         <input type='text' name="lastname" required/>
         <input type='email' name="email" required/>
