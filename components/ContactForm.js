29fa4c5c34eeac68f2e6959a7ba8efda8dd88ec6
Haig McGarry <mcgarry.haig@gmail.com>
2026-01-09 15:07:00 -0500

    added placeholder text to homepage, added formsubmit stuff w/ honeypot

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,10 +1,15 @@
 export default function ContactForm(){
     return(
     <form action="https://formsubmit.co/e1ce23fb350fd46fa60e91af659afd3b" method='POST'>
+
+        <input type="hidden" name="_subject" value="Website Email" />
+        <input type="text" name="_honey" style="display:none" />
+        <input type="hidden" name="_captcha" value="false" />
+
         <input type="text" name="firstname" required/>
         <input type='text' name="lastname" required/>
         <input type='email' name="email" required/>
-        <textarea name='comment'></textarea>
+        <textarea name='comment' placeholder="say something nice :)"></textarea>
         <button type="submit">Secondary</button>
     </form>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import WebNavbar from "../components/Navbar";
-import {Row, Container, Col} from "react-bootstrap";
 import Head from "next/head";
-import BookCard from "@/components/BookCard";
 
 export default function Home(){
   return (
@@ -12,6 +10,11 @@ export default function Home(){
         <meta name="homepage"></meta>
       </Head>
       {/**TODO: Add homepage body component*/}
+      <article>
+        Hello, my name is Haig McGarry and this is a website. My website. It&quot;s still a work in progress <br />
+        Please check out the other tabs I have built. The contact me is still a wip so its a bit ugly but Functional <br />
+        I plan to update this website with projects I work on, including other web apps and other stuff. It will be like a virtual resume but more fun
+      </article>
     </>
   )
 }
