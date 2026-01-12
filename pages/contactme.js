6cedd09fd4f947e23f91533e742bd6924d3889bb
hmcgarry <mcgarry.haig@gmail.com>
2026-01-12 17:24:38 -0500

    Added a Head element to contact

--- pages/contactme.js
+++ pages/contactme.js
@@ -1,9 +1,14 @@
-import "../styles/hmnavbar.module.css"
+import styles from "../styles/hmncard.module.css"
 import WebNavbar from "../components/Navbar"
 import ContactForm from "@/components/ContactForm"
+import Head from "next/head"
 export default function ContactMe(){
         return (
         <>
+        <Head>
+                <title>Haig &quot;Contact Me&quot; McGarry</title>
+                <meta name="contactpage"></meta>    
+        </Head>
             <WebNavbar />
                  <article className="HMBody">
                    <ContactForm />
